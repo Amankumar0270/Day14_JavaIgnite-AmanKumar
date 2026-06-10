53b70c630080560f6c9cebfe3f74efbd3f508db2
rite2harini <rite2harini@gmail.com>
2026-06-10 15:09:35 +0530

    Add Stack Browser Back Button Simulation
    Implement a simulation of a browser back button using a stack to manage visited pages.

---
+++ Stack_Browser_Back_Button_Simulation.java
@@ -0,0 +1,15 @@
+/*
+A browser stores visited pages in a Stack.
+
+Requirements:
+Push 5 website names into a Stack.
+Display all visited websites.
+Simulate pressing the Back button twice.
+Display the pages that were removed.
+Display the current page after going back.
+Concepts Tested:
+Stack
+push()
+pop()
+peek()
+*/
